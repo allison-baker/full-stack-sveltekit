35af2286f37e5f1421bc068df2a8d7deb5872c39
allison-baker <baker.allison.m@gmail.com>
2024-01-14 13:47:41 -0700

    fix mongodb connection

--- src/routes/mongodb/+page.server.ts
+++ src/routes/mongodb/+page.server.ts
@@ -1,9 +1,8 @@
 import { MongoClient, ServerApiVersion } from 'mongodb';
-
-const uri = process.env.MONGODB_URI || 'mongodb://localhost:27017';
+const URI = process.env.MONGODB_URI;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
-const client = new MongoClient(uri, {
+const client = new MongoClient(URI, {
 	serverApi: {
 		version: ServerApiVersion.v1,
 		strict: true,
