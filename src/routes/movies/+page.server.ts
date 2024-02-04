6fc3e07aede33e8a2b4080a2c7842fee6a0d4560
allison-baker <baker.allison.m@gmail.com>
2024-02-04 02:59:30 -0700

    start movies database

---
+++ src/routes/movies/+page.server.ts
@@ -0,0 +1,28 @@
+import { MongoClient, ObjectId } from 'mongodb';
+
+if (!process.env.MONGODB_URI) {
+	throw new Error('Invalid/Missing environment variable: "MONGODB_URI"');
+}
+
+const uri = process.env.MONGODB_URI;
+const client = new MongoClient(uri);
+
+export async function load() {
+    let movies
+	try {
+		const database = client.db('sample_mflix')
+        const collection = database.collection('movies')
+
+        const moviesArr = await collection.find({ year: 1985 }).toArray()
+        movies = moviesArr.map(movie => {
+            return { ...movie, _id: (movie._id as ObjectId).toString() }
+        })
+        console.log(`Found ${moviesArr.length} movies.`)
+	} finally {
+		await client.close()
+	}
+	return {
+		status: 200,
+		body: movies
+	}
+}
---
+++ src/routes/movies/+page.svelte
@@ -0,0 +1,10 @@
+<script lang='ts'>
+    export let data: any
+    $: console.log(data)
+</script>
+
+<main>
+    <div>
+        <h3>Test</h3>
+    </div>
+</main>
