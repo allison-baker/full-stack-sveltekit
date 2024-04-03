26bd1147266fa50db754537e5557d1804f0aea40
allison-baker <baker.allison.m@gmail.com>
2024-04-03 17:31:47 -0600

    work on chart

--- src/routes/sales/+page.svelte
+++ src/routes/sales/+page.svelte
@@ -1,5 +1,7 @@
 <script lang="ts">
 	import { ListBox, ListBoxItem } from '@skeletonlabs/skeleton';
+	import Chart from 'chart.js/auto';
+	import { onMount } from 'svelte';
 
 	let allSalesData: any = [];
 	$: uniqueStoreLocations = [...new Set(allSalesData.map((sale: any) => sale.storeLocation))];
@@ -12,14 +14,51 @@
 
 	let singleLocation: string = '';
 
+	Chart.defaults.backgroundColor = '#45484a';
+	let canvasElement: HTMLCanvasElement;
+	let ctx: CanvasRenderingContext2D | null;
+
+	onMount(() => (ctx = canvasElement.getContext('2d')));
+
+	function graphSalesData() {
+		if (ctx && allSalesData.length > 0) {
+			new Chart(ctx, {
+				type: 'bar',
+				data: {
+					labels: uniqueStoreLocations,
+					datasets: [
+						{
+							label: '# of Sales',
+							data: uniqueStoreLocations.map((location: any) => totalSalesByLocation(location)),
+							borderWidth: 1,
+							backgroundColor: 'rgba(38, 80, 59, 0.5)'
+						}
+					]
+				},
+				options: {
+					scales: {
+						y: { beginAtZero: true }
+					}
+				}
+			});
+		}
+	}
+
+	function totalSalesByLocation(location: string) {
+		return allSalesData.filter((sale: any) => sale.storeLocation === location).length;
+	}
+
 	async function getSalesData() {
 		const response = await fetch('/api/sales');
 		const data = await response.json();
 		allSalesData = data;
 	}
 
-	async function postSalesData() {
-		// post
+	let statsTitle = '[Please GET Sales Data]'
+	$: {
+		if (allSalesData.length === 0) statsTitle = '[Please GET Sales Data]'
+		else if (singleLocation === '') statsTitle = '[Please Select a Location]'
+		else statsTitle = `${singleLocation} Sales Stats`
 	}
 
 	function handleLocationSelection(event: any) {
@@ -55,10 +94,9 @@
 
 <div class="max-w-[1200px] mx-auto">
 	<h1 class="m-4 text-3xl uppercase pb-2 border-b-2 border-primary-600-300-token">Sales Data</h1>
-	<button class="m-4 btn variant-ghost-surface" on:click={getSalesData}>GET Sales Data</button>
-	<button class="m-4 btn variant-ghost-surface" on:click={postSalesData}>POST Sales Data</button>
-	<div class="m-4 flex flex-wrap gap-4">
-		<ListBox class="w-48">
+	<button class="m-4 btn variant-ghost-surface w-36" on:click={getSalesData}>GET Sales Data</button>
+	<div class="m-4 flex gap-4">
+		<ListBox class="w-48 bg-surface-100-800-token rounded-md shadow-md">
 			{#each uniqueStoreLocations as location}
 				<ListBoxItem
 					class="text-center"
@@ -69,16 +107,27 @@
 				>
 			{/each}
 		</ListBox>
-		<div>
-			<h2 class="text-2xl">Sales Stats for {singleLocation}</h2>
-			<p>Total Items Sold: {salesStats.totalItemsSold}</p>
-			<p>Total Sales: {salesStats.totalSales}</p>
-			<p>Average Sales: {salesStats.averageSales}</p>
+		<div class="bg-surface-100-800-token p-4 rounded-md shadow-md flex flex-col justify-between">
+			<section>
+				<h2 class="text-xl font-bold mb-2">{statsTitle}</h2>
+				<p class="text-sm mb-1">Total Items Sold: {salesStats.totalItemsSold}</p>
+				<p class="text-sm mb-1">Total Sales: {salesStats.totalSales}</p>
+				<p class="text-sm mb-4">Average Sales: {salesStats.averageSales}</p>
+			</section>
 			<button
-				class="btn btn-sm variant-filled-primary"
+				class="btn variant-ghost-primary max-w-48"
 				disabled={!singleLocation}
 				on:click={exportSalesData}>Export Sales Data</button
 			>
 		</div>
+	</div>
+
+	<button
+		class="m-4 btn variant-ghost-surface w-36"
+		on:click={graphSalesData}
+		disabled={allSalesData.length === 0}>Graph All Sales Data</button
+	>
+	<div class="w-1/2 m-4">
+		<canvas bind:this={canvasElement}></canvas>
 	</div>
 </div>
